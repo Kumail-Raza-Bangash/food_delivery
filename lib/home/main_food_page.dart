35a7937b300c81b621460800fe81fea02858359f
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-02-15 12:44:45 +0500

    Update Header

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: const EdgeInsets.only(top: 45, bottom: 15),
+              padding: const EdgeInsets.only(right: 20, left: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Pakistan", color: AppColors.mainColor,),
+                      Row(
+                        children: [
+                          SmallText(text: "Punjab", color: Colors.black45,),
+                          const Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                      child: Icon(Icons.search, color: Colors.white,),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
