cc4e30dcb0a76309d3619e5c8298681b96cd6164
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-02-16 10:36:59 +0500

    Popular Text

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -63,6 +63,28 @@ class _FoodPageBodyState extends State<FoodPageBody> {
           ),
         ),
         
+        SizedBox( height: Dimensions.height30,),
+
+        //Popular Text
+        Container(
+          margin: EdgeInsets.only(left: Dimensions.width30,),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              BigText(text: "Popular"),
+              SizedBox(width: Dimensions.width10,),
+              Container(
+                margin: const EdgeInsets.only(bottom: 3),
+                child: BigText(text: ".", color: Colors.black26,),
+              ),
+              SizedBox(width: Dimensions.width10,),
+              Container(
+                margin: const EdgeInsets.only(bottom: 2),
+                child: BigText(text: "Food pairing"),
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
