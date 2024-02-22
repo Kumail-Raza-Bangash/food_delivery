fc693b9cbd2f87a81e27e09ef7e45f42c82986f0
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-02-22 12:14:57 +0500

    Update Cart Page

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimensions.dart';
+import 'package:food_delivery/widgets/app_icon.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -8,7 +11,39 @@ class CartPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          
+          Positioned(
+            top: Dimensions.height20*3,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(
+                  icon: Icons.arrow_back_ios,
+                  iconColor: Colors.white,
+                  backgroundColor: AppColors.mainColor,
+                  iconSize: Dimensions.iconSize24,
+                ),
+
+                SizedBox(width: Dimensions.width20*5,),
+
+                AppIcon(
+                  icon: Icons.home_outlined,
+                  iconColor: Colors.white,
+                  backgroundColor: AppColors.mainColor,
+                  iconSize: Dimensions.iconSize24,
+                ),
+
+                AppIcon(
+                  icon: Icons.shopping_cart,
+                  iconColor: Colors.white,
+                  backgroundColor: AppColors.mainColor,
+                  iconSize: Dimensions.iconSize24,
+                ),
+
+              ],
+            )
+          ),
         ],
       ),
     );
