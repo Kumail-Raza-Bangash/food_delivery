56168c3146ef41fb28cb1ec6ce6d0daf8e803db9
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-02-27 16:11:54 +0500

    Updated Code Cart Hsitory Lists

--- lib/pages/cart/cart_history.dart
+++ lib/pages/cart/cart_history.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/controllers/cart_controller.dart';
+import 'package:food_delivery/utils/app_constants.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/utils/dimensions.dart';
 import 'package:food_delivery/widgets/app_icon.dart';
@@ -29,9 +30,9 @@ class CartHistory extends StatelessWidget {
         return cartItemsPerOrder.entries.map((e) => e.value).toList();
       }
       
-      List<int> orderTimes = cartOrderTimeToList();
+      List<int> itemsPerOrder = cartOrderTimeToList();
       
-      var saveCounter = 0;  
+      var listCounter = 0;  
   
 
     return Scaffold(
@@ -48,6 +49,67 @@ class CartHistory extends StatelessWidget {
                 BigText(text: "Cart History", color: Colors.white,),
                 const AppIcon(icon: Icons.shopping_cart_outlined, iconColor: AppColors.mainColor,)
               ],
+            ),
+          ),
+
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(
+                top: Dimensions.height20,
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+              ),
+            
+              child: MediaQuery.removePadding(
+                removeTop: true,
+                context: context,
+                child: ListView(
+                  children: [
+                    for(int i=0; i<itemsPerOrder.length; i++)
+                      Container(
+                        margin: EdgeInsets.only(bottom: Dimensions.height20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            BigText(text: "Time"),
+                            SizedBox(height: Dimensions.height10,),
+                            Row(
+                              children: [
+                                Wrap(
+                                  direction: Axis.horizontal,
+                                  children: List.generate(itemsPerOrder[i], (index) {
+                                    if(listCounter<getCartHistoryList.length){
+                                      listCounter++;
+                                    }
+                                    return index <= 2 ? 
+                                    Container(
+                                      height: Dimensions.height20*4,
+                                      width: Dimensions.width20*4,
+                                      margin: EdgeInsets.only(right: Dimensions.width10/2),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(Dimensions.radius15/2),
+                                        image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(
+                                            AppConstant.BASE_URL+AppConstant.UPLOAD_URL+getCartHistoryList[listCounter-1].img!,
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                    :
+                                    Container();
+                                  }),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    
+                  ],
+                  
+                ),
+              ),
             ),
           )
         ],
