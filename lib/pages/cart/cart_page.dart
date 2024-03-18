d4c2e8eaa9be7dadec25fdb8b494bc97e0190338
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-03-18 13:07:37 +0500

    Updated Code for the Google Map

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery/base/no_data_page.dart';
 import 'package:food_delivery/controllers/auth_controller.dart';
 import 'package:food_delivery/controllers/cart_controller.dart';
+import 'package:food_delivery/controllers/location_controller.dart';
 import 'package:food_delivery/controllers/popular_product_controller.dart';
 import 'package:food_delivery/controllers/recommended_product_controller.dart';
 import 'package:food_delivery/routes/route_helper.dart';
@@ -247,7 +248,10 @@ class CartPage extends StatelessWidget {
             GestureDetector(
               onTap: () {
                 if(Get.find<AuthController>().userLoggedIn()){
-                  cartController.addToHistory();
+                  //cartController.addToHistory();
+                  if(Get.find<LocationController>().addressList.isEmpty){
+                    Get.toNamed(RouteHelper.getAddressPage());
+                  }
                 }
                 else{
                   Get.toNamed(RouteHelper.getSinginPage());
--- lib/routes/route_helper.dart
+++ lib/routes/route_helper.dart
@@ -1,3 +1,4 @@
+import 'package:food_delivery/pages/address/add_address_page.dart';
 import 'package:food_delivery/pages/auth/sign_in_page.dart';
 import 'package:food_delivery/pages/cart/cart_page.dart';
 import 'package:food_delivery/pages/food/popular_food_detail.dart';
@@ -13,6 +14,7 @@ class RouteHelper {
   static const String recommendedFood= '/recommended-food';
   static const String cartPage = '/cart-page';
   static const String signIn = '/sign-in';
+  static const String addAddress = '/add-address';
 
   static String getInitial() => initial;
   static String getSplashPage() => splashPage;
@@ -20,6 +22,7 @@ class RouteHelper {
   static String getRecommendedFood(int pageId, String page) => '$recommendedFood?pageId=$pageId&page=$page';
   static String getCartPage() => cartPage;
   static String getSinginPage() => signIn;
+  static String getAddressPage() => addAddress;
 
 
   static List<GetPage> routes = [
@@ -50,6 +53,12 @@ class RouteHelper {
       return const CartPage();
     },
     transition: Transition.upToDown,
+    ),
+
+    GetPage(name: addAddress, page: () {
+      return const AddAddressPage();
+    },
+    transition: Transition.fadeIn,
     ),
 
   ];
